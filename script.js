f5bc00277df3a118d6f9628e8d5db0879813d9bd
Sabari Vasagan <sabari4380@gmail.com>
2026-01-05 17:47:17 +0530

    123

---
+++ script.js
@@ -0,0 +1,92 @@
+
+
+// // 9,
+//   let rev = [1,2,3,4,5];
+//   let reversed = rev.reverse();
+//  console.log(reversed);
+
+// // 10,
+
+
+// // 11,
+// function square(num){
+// return num * num;
+// }
+// console.log(square(8));
+
+// // 12,
+// function max(a, b){
+// return Math. max(a, b)
+// }
+// console.log(max(100,200));
+
+// // 13,
+// function factorial(n){
+// if ( n < 0)return "Factorial not defined for negative numbers";
+
+// let result = 2;
+// for (let i =2; i <= n; i++) {
+
+//   result *=i;
+// }
+// return result;
+
+// }
+// console.log(factorial(4));
+
+
+// // 14,
+// // function isprime(num4);
+// // {
+// // if ( num4 <= 1) return false;
+// // for ( let i=3; i < num4; i++){
+
+// //   if (num4 %  i===0){
+
+// //     return false;
+// //   }
+// // }
+// // return true;
+// // }
+// // console.log(isprime(7));
+// // console.log(isprime(10));
+
+// // 15,
+// let arr = ["apple","orange","graps","banana"]
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[0]);
+
+
+// //1 
+// const a = [1, 2];
+// const b = [  a,3,4];
+// console.log(b);
+
+// const a = [1, 2];
+// const b = a;
+// const c = [...a];
+
+
+// b.push(3);
+// console.log(a, c)
+
+// clonedarray.push(4);
+// console.log("original:",originalarray);
+// console.log("clone:",clonedarray);
+// let arr = [100,200,300,400]
+// let poppedval = arr.pop()
+// console.log(poppedval, arr);
+// arr.push(500,550,600)
+// console.log(arr);
+
+// let firstval = arr.shift()
+// console.log( firstval, arr );
+// arr.unshift( -1, -2, 0, 10, 20 )
+// console.log(arr);
+
+// let newarray = [10,20,30,40]
+// newarray.splice(1,2, ("newval",true,[11,12]),"secondval")
+// console.log( newarray);
+
+// newarray.splice( 0, 0, 100, 200, 300 )
